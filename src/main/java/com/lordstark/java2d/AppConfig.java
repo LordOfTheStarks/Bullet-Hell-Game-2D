08ee012a8de2384f93b41696ba7b740b52412901
Boray Celik <boray.06@hotmail.com>
2024-10-19 22:29:28 +0200

    Version 1.01 of Bullet Hell Game

--- src/main/java/com/lordstark/java2d/AppConfig.java
+++ src/main/java/com/lordstark/java2d/AppConfig.java
@@ -1,4 +1,19 @@
 package com.lordstark.java2d;
 
 public class AppConfig {
+    private static int width = 1280;
+    private static int height = 720;
+
+    public static int getWidth() {
+        return width;
+    }
+    public static void setWidth(int width) {
+        AppConfig.width = width;
+    }
+    public static int getHeight() {
+        return height;
+    }
+    public static void setHeight(int height) {
+        AppConfig.height = height;
+    }
 }
